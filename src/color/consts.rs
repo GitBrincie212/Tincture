90686d99b530cb2cd4dc8016a38e9259cf3bffee
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2024-11-01 21:41:56 +0200

    Some structure changes to the code

---
+++ src/color/consts.rs
@@ -0,0 +1,227 @@
+use crate::color;
+
+pub const WHITE: color::Color = color::Color {
+    r: 255,
+    g: 255,
+    b: 255,
+    a: 255,
+};
+pub const BLACK: color::Color = color::Color {
+    r: 0,
+    g: 0,
+    b: 0,
+    a: 255,
+};
+pub const RED: color::Color = color::Color {
+    r: 255,
+    g: 0,
+    b: 0,
+    a: 255,
+};
+pub const BLUE: color::Color = color::Color {
+    r: 0,
+    g: 0,
+    b: 255,
+    a: 255,
+};
+pub const GREEN: color::Color = color::Color {
+    r: 0,
+    g: 255,
+    b: 0,
+    a: 255,
+};
+pub const YELLOW: color::Color = color::Color {
+    r: 255,
+    g: 255,
+    b: 0,
+    a: 255,
+};
+pub const CYAN: color::Color = color::Color {
+    r: 0,
+    g: 255,
+    b: 255,
+    a: 255,
+};
+pub const MAGENTA: color::Color = color::Color {
+    r: 255,
+    g: 0,
+    b: 255,
+    a: 255,
+};
+pub const PINK: color::Color = color::Color {
+    r: 255,
+    g: 0,
+    b: 125,
+    a: 255,
+};
+pub const PURPLE: color::Color = color::Color {
+    r: 125,
+    g: 0,
+    b: 255,
+    a: 255,
+};
+pub const ORANGE: color::Color = color::Color {
+    r: 255,
+    g: 125,
+    b: 0,
+    a: 255,
+};
+pub const LIME: color::Color = color::Color {
+    r: 125,
+    g: 255,
+    b: 0,
+    a: 255,
+};
+pub const TEAL: color::Color = color::Color {
+    r: 0,
+    g: 255,
+    b: 125,
+    a: 255,
+};
+pub const VIVID_BLUE: color::Color = color::Color {
+    r: 0,
+    g: 125,
+    b: 255,
+    a: 255,
+};
+pub const LIGHT_RED: color::Color = color::Color {
+    r: 255,
+    g: 125,
+    b: 125,
+    a: 255,
+};
+pub const LIGHT_GREEN: color::Color = color::Color {
+    r: 125,
+    g: 255,
+    b: 125,
+    a: 255,
+};
+pub const LIGHT_BLUE: color::Color = color::Color {
+    r: 125,
+    g: 125,
+    b: 255,
+    a: 255,
+};
+pub const LIGHT_CYAN: color::Color = color::Color {
+    r: 125,
+    g: 255,
+    b: 255,
+    a: 255,
+};
+pub const LIGHT_MAGENTA: color::Color = color::Color {
+    r: 255,
+    g: 125,
+    b: 255,
+    a: 255,
+};
+pub const LIGHT_YELLOW: color::Color = color::Color {
+    r: 255,
+    g: 255,
+    b: 125,
+    a: 255,
+};
+
+pub const LIGHT_PURPLE: color::Color = color::Color {
+    r: 203,
+    g: 195,
+    b: 227,
+    a: 255
+};
+
+pub const LIGHT_LIME: color::Color = color::Color {
+    r: 174,
+    g: 253,
+    b: 108,
+    a: 255
+};
+pub const LIGHT_ORANGE: color::Color = color::Color {
+    r: 255,
+    g: 213,
+    b: 128,
+    a: 255
+};
+pub const LIGHT_TEAL: color::Color = color::Color {
+    r: 144,
+    g: 228,
+    b: 193,
+    a: 255
+};
+pub const LIGHT_PINK: color::Color = color::Color {
+    r: 255,
+    g: 182,
+    b: 193,
+    a: 255
+};
+
+pub const DARK_RED: color::Color = color::Color {
+    r: 125,
+    g: 62,
+    b: 62,
+    a: 255,
+};
+pub const DARK_BLUE: color::Color = color::Color {
+    r: 62,
+    g: 62,
+    b: 125,
+    a: 255,
+};
+pub const DARK_GREEN: color::Color = color::Color {
+    r: 62,
+    g: 125,
+    b: 62,
+    a: 255,
+};
+pub const DARK_YELLOW: color::Color = color::Color {
+    r: 125,
+    g: 125,
+    b: 62,
+    a: 255,
+};
+pub const DARK_CYAN: color::Color = color::Color {
+    r: 62,
+    g: 125,
+    b: 125,
+    a: 255,
+};
+pub const DARK_MAGENTA: color::Color = color::Color {
+    r: 125,
+    g: 62,
+    b: 125,
+    a: 255,
+};
+pub const DARK_ORANGE: color::Color = color::Color {
+    r: 125,
+    g: 93,
+    b: 62,
+    a: 255,
+};
+pub const DARK_PINK: color::Color = color::Color {
+    r: 125,
+    g: 62,
+    b: 93,
+    a: 255,
+};
+pub const DARK_PURPLE: color::Color = color::Color {
+    r: 93,
+    g: 62,
+    b: 125,
+    a: 255,
+};
+pub const DARK_LIME: color::Color = color::Color {
+    r: 93,
+    g: 125,
+    b: 62,
+    a: 255,
+};
+pub const DARK_TEAL: color::Color = color::Color {
+    r: 62,
+    g: 125,
+    b: 93,
+    a: 255,
+};
+pub const DEFAULT_BACKGROUND: color::Color = color::Color {
+    r: 27,
+    g: 29,
+    b: 33,
+    a: 255,
+};
--- tincture/color.pyi
+++ tincture/color.pyi
@@ -1,39 +1,4 @@
 class Color:
-    WHITE: Color = Color(255, 255, 255, 255)
-    BLACK: Color = Color(0, 0, 0, 255)
-    RED: Color = Color(255, 0, 0, 255)
-    BLUE: Color = Color(0, 0, 255, 255)
-    GREEN: Color = Color(0, 255, 0, 255)
-    YELLOW: Color = Color(255, 255, 0, 255)
-    CYAN: Color = Color(0, 255, 255, 255)
-    MAGENTA: Color = Color(255, 0, 255, 255)
-    PINK: Color = Color(255, 0, 125, 255)
-    PURPLE: Color = Color(125, 0, 255, 255)
-    ORANGE: Color = Color(255, 125, 0, 255)
-    LIME: Color = Color(125, 255, 0, 255)
-    TEAL: Color = Color(0, 255, 125, 255)
-    VIVID_BLUE: Color = Color(0, 125, 255, 255)
-    LIGHT_RED: Color = Color(255, 125, 125, 255)
-    LIGHT_GREEN: Color = Color(125, 255, 125, 255)
-    LIGHT_BLUE: Color = Color(125, 125, 255, 255)
-    LIGHT_CYAN: Color = Color(125, 255, 255, 255)
-    LIGHT_MAGENTA: Color = Color(255, 125, 255, 255)
-    LIGHT_YELLOW: Color = Color(255, 255, 125, 255)
-    DARK_RED: Color = Color(125, 62, 62, 255)
-    DARK_BLUE: Color = Color(62, 62, 125, 255)
-    DARK_GREEN: Color = Color(62, 125, 62, 255)
-    DARK_YELLOW: Color = Color(125, 125, 62, 255)
-    DARK_CYAN: Color = Color(62, 125, 125, 255)
-    DARK_MAGENTA: Color = Color(125, 62, 125, 255)
-    DARK_ORANGE: Color = Color(125, 93, 62, 255)
-    DARK_PINK: Color = Color(125, 62, 93, 255)
-    DARK_PURPLE: Color = Color(93, 62, 125, 255)
-    DARK_LIME: Color = Color(93, 125, 62, 255)
-    DARK_TEAL: Color = Color(62, 125, 93, 255)
-
-    DEFAULT_BACKGROUND: Color = Color(27, 29, 33, 255)
-
-
     def __init__(self, r: int, g: int, b: int, a: int = 255) -> None:
         """
         Color stores RGBA values that can be freely manipulated with all sorts of operation.
@@ -45,6 +10,11 @@ class Color:
         :param b: The Blue Component Of The Color.
         :param a: The Alpha Component Of The Color
         """
+        self.r = r
+        self.g = g
+        self.b = b
+        self.a = a
+        ...
 
     @staticmethod
     def from_srgb(r: int, g: int, b: int) -> "Color":
@@ -582,3 +552,42 @@ class Color:
     def __setitem__(self, a: str | int, b: int) -> int: ...
     def __rshift__(self, places: int) -> "Color": ...
     def __lshift__(self, places: int) -> "Color": ...
+
+WHITE: Color = Color(255, 255, 255, 255)
+BLACK: Color = Color(0, 0, 0, 255)
+RED: Color = Color(255, 0, 0, 255)
+BLUE: Color = Color(0, 0, 255, 255)
+GREEN: Color = Color(0, 255, 0, 255)
+YELLOW: Color = Color(255, 255, 0, 255)
+CYAN: Color = Color(0, 255, 255, 255)
+MAGENTA: Color = Color(255, 0, 255, 255)
+PINK: Color = Color(255, 0, 125, 255)
+PURPLE: Color = Color(125, 0, 255, 255)
+ORANGE: Color = Color(255, 125, 0, 255)
+LIME: Color = Color(125, 255, 0, 255)
+TEAL: Color = Color(0, 255, 125, 255)
+VIVID_BLUE: Color = Color(0, 125, 255, 255)
+LIGHT_RED: Color = Color(255, 125, 125, 255)
+LIGHT_GREEN: Color = Color(125, 255, 125, 255)
+LIGHT_BLUE: Color = Color(125, 125, 255, 255)
+LIGHT_CYAN: Color = Color(125, 255, 255, 255)
+LIGHT_MAGENTA: Color = Color(255, 125, 255, 255)
+LIGHT_YELLOW: Color = Color(255, 255, 125, 255)
+LIGHT_PURPLE: Color = Color(203, 195, 227, 255)
+LIGHT_LIME: Color = Color(174, 253, 108, 255)
+LIGHT_ORANGE: Color = Color(255, 213, 128, 255)
+LIGHT_TEAL: Color = Color(144, 228, 193, 255)
+LIGHT_PINK: Color = Color(255, 182, 193, 255)
+DARK_RED: Color = Color(125, 62, 62, 255)
+DARK_BLUE: Color = Color(62, 62, 125, 255)
+DARK_GREEN: Color = Color(62, 125, 62, 255)
+DARK_YELLOW: Color = Color(125, 125, 62, 255)
+DARK_CYAN: Color = Color(62, 125, 125, 255)
+DARK_MAGENTA: Color = Color(125, 62, 125, 255)
+DARK_ORANGE: Color = Color(125, 93, 62, 255)
+DARK_PINK: Color = Color(125, 62, 93, 255)
+DARK_PURPLE: Color = Color(93, 62, 125, 255)
+DARK_LIME: Color = Color(93, 125, 62, 255)
+DARK_TEAL: Color = Color(62, 125, 93, 255)
+
+DEFAULT_BACKGROUND: Color = Color(27, 29, 33, 255)
