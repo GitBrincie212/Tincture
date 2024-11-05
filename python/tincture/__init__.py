9faca155ed618958d3df182089564f757797e77c
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2024-11-05 15:34:46 +0200

    Moved the stub files to a python directory

---
+++ python/tincture/__init__.py
@@ -0,0 +1,40 @@
+from tincture import Color
+from ._blending import *
+from ._color import *
+
+WHITE: Color
+BLACK: Color
+RED: Color
+BLUE: Color
+GREEN: Color
+YELLOW: Color
+CYAN: Color
+MAGENTA: Color
+PINK: Color
+PURPLE: Color
+ORANGE: Color
+LIME: Color
+TEAL: Color
+VIVID_BLUE: Color
+LIGHT_RED: Color
+LIGHT_GREEN: Color
+LIGHT_BLUE: Color
+LIGHT_CYAN: Color
+LIGHT_MAGENTA: Color
+LIGHT_YELLOW: Color
+LIGHT_PURPLE: Color
+LIGHT_LIME: Color
+LIGHT_ORANGE: Color
+LIGHT_TEAL: Color
+LIGHT_PINK: Color
+DARK_RED: Color
+DARK_BLUE: Color
+DARK_GREEN: Color
+DARK_YELLOW: Color
+DARK_CYAN: Color
+DARK_MAGENTA: Color
+DARK_ORANGE: Color
+DARK_PINK: Color
+DARK_PURPLE: Color
+DARK_LIME: Color
+DARK_TEAL: Color
