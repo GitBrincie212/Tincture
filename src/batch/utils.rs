66d95d32bd8c47905bfeea4070d16833b5fca78a
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-07-19 12:23:15 +0300

    Added some utilities for handling the batching operations

---
+++ src/batch/utils.rs
@@ -0,0 +1,43 @@
+#[macro_export]
+macro_rules! scalar_to_packed {
+    ($scalars: expr, $identity_fn: expr) => {{
+        $scalars
+            .par_iter()
+            .map(|x| {
+                f64x4::from_array([
+                    *x,
+                    *x,
+                    *x,
+                    $identity_fn(x)
+                ])
+            })
+            .collect()
+    }};
+}
+
+#[macro_export]
+macro_rules! color_to_packed {
+    ($colors: expr, $identity_fn: expr) => {{
+        $colors.as_slice()
+            .par_iter()
+            .map(|x| {
+                f64x4::from_array(
+                    extract_rgba_channels_by_type!(x, f64, $identity_fn)
+                )
+            })
+            .collect()
+    }};
+}
+
+#[macro_export]
+macro_rules! handle_lower_operation {
+    ($self_lanes: expr, $other: expr, $operation: tt) => {
+        $self_lanes
+            .par_iter_mut()
+            .for_each(|lane| {
+                for src in &$other {
+                    *lane $operation *src;
+                }
+            });
+    };
+}
