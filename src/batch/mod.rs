b5c6baa3a0bef824d248b5333bc4122d75bd9916
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-07-20 11:21:19 +0300

    Finished most of the batch instructions, added __getitem__ for getting a color and __setitem__ for setting a color

--- src/batch/mod.rs
+++ src/batch/mod.rs
@@ -1,14 +1,15 @@
+use std::ops::{Add, Div, Mul, Sub};
 use std::simd::{f64x4, StdFloat};
 use std::simd::prelude::SimdFloat;
-use std::sync::atomic::{Ordering};
+use std::sync::atomic::{AtomicU32, Ordering};
 use std::sync::{Arc, RwLock};
 use crossbeam::queue::SegQueue;
-use pyo3::{pyclass, pymethods};
+use pyo3::{exceptions, pyclass, pymethods};
 use pyo3::prelude::*;
 use crate::batch::batch_instructions::{BatchInstructionSet};
 use crate::color::Color;
 use rayon::prelude::*;
-use crate::{color_to_packed, extract_rgba_channels_by_type, handle_lower_operation, scalar_to_packed};
+use crate::{color_to_packed, create_color, extract_rgba_channels_by_type, handle_lower_operation, scalar_to_packed};
 
 mod utils;
 mod batch_instructions;
@@ -38,20 +39,32 @@ impl ColorBatch {
         }).collect::<Vec<_>>();
         while let Some(instruction_type) = self.queue.pop() {
             match instruction_type {
-                BatchInstructionSet::AddBatches(values) => {}
-                BatchInstructionSet::SubBatches(_) => {}
-                BatchInstructionSet::TensorBatches(other) => {}
                 BatchInstructionSet::LowerAddend(other) => {
-                    handle_lower_operation!(self_lanes, other, +=);
+                    handle_lower_operation!(self_lanes, other, add);
                 }
                 BatchInstructionSet::LowerSub(other) => {
-                    handle_lower_operation!(self_lanes, other, -=);
+                    handle_lower_operation!(self_lanes, other, sub);
                 }
                 BatchInstructionSet::LowerMul(other) => {
-                    handle_lower_operation!(self_lanes, other, *=);
+                    handle_lower_operation!(self_lanes, other, mul);
                 }
-                BatchInstructionSet::DivScalarToBatch(_) => {}
-                BatchInstructionSet::NthRootScalarToBatch(_) => {}
+                BatchInstructionSet::DivScalarToBatch(other) => {
+                    handle_lower_operation!(self_lanes, other, div);
+                }
+
+                BatchInstructionSet::NthRootScalarToBatch(other) => {
+                    self_lanes
+                        .par_iter_mut()
+                        .for_each(|lane| {
+                            for src in &other {
+                                // Fallback to scalar, as its impossible to do with SIMD
+                                lane[0] = lane[0].powf(1.0 / src[0]);
+                                lane[1] = lane[1].powf(1.0 / src[1]);
+                                lane[2] = lane[2].powf(1.0 / src[2]);
+                                lane[3] = lane[3].powf(1.0 / src[3]);
+                            }
+                        });
+                }
             }
         }
         *batch_write = self_lanes.into_par_iter().map(|x| {
@@ -120,11 +133,49 @@ impl ColorBatch {
         slf
     }
 
+    #[pyo3(signature = (scalars, include_transparency=true))]
+    pub fn div_scalar(slf: PyRef<'_, Self>, scalars: Vec<f64>, include_transparency: bool) -> PyRef<'_, Self> {
+        let packed: Vec<f64x4> = scalar_to_packed!(scalars, |x| x * f64::from(include_transparency));
+        slf.queue.push(BatchInstructionSet::DivScalarToBatch(packed));
+        slf
+    }
+
+    #[pyo3(signature = (scalars, include_transparency=true))]
+    pub fn nth_root_scalar(slf: PyRef<'_, Self>, scalars: Vec<f64>, include_transparency: bool) -> PyRef<'_, Self> {
+        let packed: Vec<[f64; 4]> = scalars
+            .par_iter()
+            .map(|x| [*x, *x, *x, x * f64::from(include_transparency)])
+            .collect();
+        slf.queue.push(BatchInstructionSet::NthRootScalarToBatch(packed));
+        slf
+    }
+
+    pub fn __str__(&self) -> String {
+        self.__repr__()
+    }
+
     pub fn __repr__(&self) -> String {
-        format!("ColorBatch({})", String::from(self.batch.read().unwrap().iter().map(|x| {
+        format!("ColorBatch({})", self.batch.read().unwrap().iter().map(|x| {
             let rgba = x.to_be_bytes();
             format!("Color({}, {}, {}, {})", rgba[0], rgba[1], rgba[2], rgba[3])
-        }).collect::<Vec<_>>().join(", ")))
+        }).collect::<Vec<_>>().join(", "))
+    }
+
+    fn __getitem__(&self, index: isize) -> PyResult<Color> {
+        let read_batch = self.batch.read().unwrap();
+        if index < 0 || index >= read_batch.len() as isize {
+            return Err(exceptions::PyIndexError::new_err("Color batch index out of range"));
+        }
+        Ok(create_color!(read_batch[index as usize]))
+    }
+
+    fn __setitem__(&self, index: isize, value: Color) -> PyResult<()> {
+        let mut write_batch = self.batch.write().unwrap();
+        if index < 0 || index >= write_batch.len() as isize {
+            return Err(exceptions::PyIndexError::new_err("Color batch index out of range"));
+        }
+        write_batch[index as usize] = value.0.load(Ordering::Relaxed);
+        Ok(())
     }
 
     pub fn operate(slf: PyRef<'_, Self>) -> PyRef<'_, Self> {
