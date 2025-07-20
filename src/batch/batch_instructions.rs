23c277ee217f702d38d610122a8e5ca5bf03fd7a
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-07-20 11:19:34 +0300

    Removed batch to batch operations (might add them later down the line) and changed some stuff

--- src/batch/batch_instructions.rs
+++ src/batch/batch_instructions.rs
@@ -1,13 +1,9 @@
 use std::simd::{f64x4};
-use crate::batch::ColorBatch;
 
 pub enum BatchInstructionSet {
-    AddBatches(Vec<ColorBatch>),
-    SubBatches(Vec<ColorBatch>),
-    TensorBatches(Vec<ColorBatch>),
     LowerAddend(Vec<f64x4>),
     LowerSub(Vec<f64x4>),
     LowerMul(Vec<f64x4>),
-    DivScalarToBatch(Vec<f64>),
-    NthRootScalarToBatch(Vec<f64>),
+    DivScalarToBatch(Vec<f64x4>),
+    NthRootScalarToBatch(Vec<[f64; 4]>),
 }
