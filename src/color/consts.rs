d8334a04060f08709a3ab7185d1cc60f64773a72
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-08-15 15:57:56 +0300

    Used static as opposed to const

--- src/color/consts.rs
+++ src/color/consts.rs
@@ -2,221 +2,221 @@ use crate::{create_color};
 use crate::color::Color;
 use crate::color::AtomicU32;
 
-pub const WHITE: Color = create_color!(u32::from_be_bytes([
+pub static WHITE: Color = create_color!(u32::from_be_bytes([
     255,
     255,
     255,
     255,
 ]));
 
-pub const BLACK: Color = create_color!(u32::from_be_bytes([
+pub static BLACK: Color = create_color!(u32::from_be_bytes([
     0,
     0,
     0,
     255,
 ]));
-pub const RED: Color = create_color!(u32::from_be_bytes([
+pub static RED: Color = create_color!(u32::from_be_bytes([
     255,
     0,
     0,
     255,
 ]));
-pub const BLUE: Color = create_color!(u32::from_be_bytes([
+pub static BLUE: Color = create_color!(u32::from_be_bytes([
     0,
     0,
     255,
     255,
 ]));
-pub const GREEN: Color = create_color!(u32::from_be_bytes([
+pub static GREEN: Color = create_color!(u32::from_be_bytes([
     0,
     255,
     0,
     255,
 ]));
-pub const YELLOW: Color = create_color!(u32::from_be_bytes([
+pub static YELLOW: Color = create_color!(u32::from_be_bytes([
     255,
     255,
     0,
     255,
 ]));
-pub const CYAN: Color = create_color!(u32::from_be_bytes([
+pub static CYAN: Color = create_color!(u32::from_be_bytes([
     0,
     255,
     255,
     255,
 ]));
-pub const MAGENTA: Color = create_color!(u32::from_be_bytes([
+pub static MAGENTA: Color = create_color!(u32::from_be_bytes([
     255,
     0,
     255,
     255,
 ]));
-pub const PINK: Color = create_color!(u32::from_be_bytes([
+pub static PINK: Color = create_color!(u32::from_be_bytes([
     255,
     0,
     125,
     255,
 ]));
-pub const PURPLE: Color = create_color!(u32::from_be_bytes([
+pub static PURPLE: Color = create_color!(u32::from_be_bytes([
     125,
     0,
     255,
     255,
 ]));
-pub const ORANGE: Color = create_color!(u32::from_be_bytes([
+pub static ORANGE: Color = create_color!(u32::from_be_bytes([
     255,
     125,
     0,
     255,
 ]));
-pub const LIME: Color = create_color!(u32::from_be_bytes([
+pub static LIME: Color = create_color!(u32::from_be_bytes([
     125,
     255,
     0,
     255,
 ]));
-pub const TEAL: Color = create_color!(u32::from_be_bytes([
+pub static TEAL: Color = create_color!(u32::from_be_bytes([
     0,
-    255,
-    125,
-    255,
-]));
-pub const VIVID_BLUE: Color = create_color!(u32::from_be_bytes([
-    0,
-    125,
-    255,
-    255,
-]));
-pub const LIGHT_RED: Color = create_color!(u32::from_be_bytes([
-    255,
-    125,
-    125,
-    255,
-]));
-pub const LIGHT_GREEN: Color = create_color!(u32::from_be_bytes([
-    125,
     255,
     125,
     255,
 ]));
-pub const LIGHT_BLUE: Color = create_color!(u32::from_be_bytes([
+pub static VIVID_BLUE: Color = create_color!(u32::from_be_bytes([
+    0,
+    125,
+    255,
+    255,
+]));
+pub static LIGHT_RED: Color = create_color!(u32::from_be_bytes([
+    255,
+    125,
+    125,
+    255,
+]));
+pub static LIGHT_GREEN: Color = create_color!(u32::from_be_bytes([
+    125,
+    255,
+    125,
+    255,
+]));
+pub static LIGHT_BLUE: Color = create_color!(u32::from_be_bytes([
     125,
     125,
     255,
     255,
 ]));
-pub const LIGHT_CYAN: Color = create_color!(u32::from_be_bytes([
+pub static LIGHT_CYAN: Color = create_color!(u32::from_be_bytes([
     125,
     255,
     255,
     255,
 ]));
-pub const LIGHT_MAGENTA: Color = create_color!(u32::from_be_bytes([
+pub static LIGHT_MAGENTA: Color = create_color!(u32::from_be_bytes([
     255,
     125,
     255,
     255,
 ]));
-pub const LIGHT_YELLOW: Color = create_color!(u32::from_be_bytes([
+pub static LIGHT_YELLOW: Color = create_color!(u32::from_be_bytes([
     255,
     255,
     125,
     255,
 ]));
 
-pub const LIGHT_PURPLE: Color = create_color!(u32::from_be_bytes([
+pub static LIGHT_PURPLE: Color = create_color!(u32::from_be_bytes([
     203,
     195,
     227,
     255,
 ]));
 
-pub const LIGHT_LIME: Color = create_color!(u32::from_be_bytes([
+pub static LIGHT_LIME: Color = create_color!(u32::from_be_bytes([
     174,
     253,
     108,
     255,
 ]));
-pub const LIGHT_ORANGE: Color = create_color!(u32::from_be_bytes([
+pub static LIGHT_ORANGE: Color = create_color!(u32::from_be_bytes([
     255,
     213,
     128,
     255,
 ]));
-pub const LIGHT_TEAL: Color = create_color!(u32::from_be_bytes([
+pub static LIGHT_TEAL: Color = create_color!(u32::from_be_bytes([
     144,
     228,
     193,
     255,
 ]));
-pub const LIGHT_PINK: Color = create_color!(u32::from_be_bytes([
+pub static LIGHT_PINK: Color = create_color!(u32::from_be_bytes([
     255,
     182,
     193,
     255,
 ]));
 
-pub const DARK_RED: Color = create_color!(u32::from_be_bytes([
+pub static DARK_RED: Color = create_color!(u32::from_be_bytes([
     125,
     62,
     62,
     255,
 ]));
-pub const DARK_BLUE: Color = create_color!(u32::from_be_bytes([
+pub static DARK_BLUE: Color = create_color!(u32::from_be_bytes([
     62,
     62,
     125,
     255,
 ]));
-pub const DARK_GREEN: Color = create_color!(u32::from_be_bytes([
+pub static DARK_GREEN: Color = create_color!(u32::from_be_bytes([
     62,
     125,
     62,
     255,
 ]));
-pub const DARK_YELLOW: Color = create_color!(u32::from_be_bytes([
+pub static DARK_YELLOW: Color = create_color!(u32::from_be_bytes([
     125,
     125,
     62,
     255,
 ]));
-pub const DARK_CYAN: Color = create_color!(u32::from_be_bytes([
+pub static DARK_CYAN: Color = create_color!(u32::from_be_bytes([
     62,
     125,
     125,
     255,
 ]));
-pub const DARK_MAGENTA: Color = create_color!(u32::from_be_bytes([
+pub static DARK_MAGENTA: Color = create_color!(u32::from_be_bytes([
     125,
     62,
     125,
     255,
 ]));
-pub const DARK_ORANGE: Color = create_color!(u32::from_be_bytes([
+pub static DARK_ORANGE: Color = create_color!(u32::from_be_bytes([
     125,
     93,
     62,
     255,
 ]));
-pub const DARK_PINK: Color = create_color!(u32::from_be_bytes([
+pub static DARK_PINK: Color = create_color!(u32::from_be_bytes([
     125,
     62,
     93,
     255,
 ]));
-pub const DARK_PURPLE: Color = create_color!(u32::from_be_bytes([
+pub static DARK_PURPLE: Color = create_color!(u32::from_be_bytes([
     93,
     62,
     125,
     255,
 ]));
-pub const DARK_LIME: Color = create_color!(u32::from_be_bytes([
+pub static DARK_LIME: Color = create_color!(u32::from_be_bytes([
     93,
     125,
     62,
     255,
 ]));
-pub const DARK_TEAL: Color = create_color!(u32::from_be_bytes([
+pub static DARK_TEAL: Color = create_color!(u32::from_be_bytes([
     62,
     125,
     93,
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-#![feature(portable_simd)]
 #![allow(unused_must_use)]
 #![allow(clippy::wrong_self_convention)]
 
@@ -12,41 +11,41 @@ fn tincture(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<color::Color>();
     m.add_class::<batch::ColorBatch>();
     m.add_class::<color::blending::BlendingMode>();
-    m.add("WHITE", color::consts::WHITE);
-    m.add("RED", color::consts::RED);
-    m.add("BLUE", color::consts::BLUE);
-    m.add("GREEN", color::consts::GREEN);
-    m.add("BLACK", color::consts::BLACK);
-    m.add("CYAN", color::consts::CYAN);
-    m.add("YELLOW", color::consts::YELLOW);
-    m.add("MAGENTA", color::consts::MAGENTA);
-    m.add("PURPLE", color::consts::PURPLE);
-    m.add("LIME", color::consts::LIME);
-    m.add("ORANGE", color::consts::ORANGE);
-    m.add("TEAL", color::consts::TEAL);
-    m.add("PINK", color::consts::PINK);
-    m.add("DARK_RED", color::consts::DARK_RED);
-    m.add("DARK_BLUE", color::consts::DARK_BLUE);
-    m.add("DARK_GREEN", color::consts::DARK_GREEN);
-    m.add("DARK_CYAN", color::consts::DARK_CYAN);
-    m.add("DARK_YELLOW", color::consts::DARK_YELLOW);
-    m.add("DARK_MAGENTA", color::consts::DARK_MAGENTA);
-    m.add("DARK_PURPLE", color::consts::DARK_PURPLE);
-    m.add("DARK_LIME", color::consts::DARK_LIME);
-    m.add("DARK_ORANGE", color::consts::DARK_ORANGE);
-    m.add("DARK_TEAL", color::consts::DARK_TEAL);
-    m.add("DARK_PINK", color::consts::DARK_PINK);
-    m.add("LIGHT_RED", color::consts::LIGHT_RED);
-    m.add("LIGHT_BLUE", color::consts::LIGHT_BLUE);
-    m.add("LIGHT_GREEN", color::consts::LIGHT_GREEN);
-    m.add("LIGHT_CYAN", color::consts::LIGHT_CYAN);
-    m.add("LIGHT_YELLOW", color::consts::LIGHT_YELLOW);
-    m.add("LIGHT_MAGENTA", color::consts::LIGHT_MAGENTA);
-    m.add("LIGHT_PURPLE", color::consts::LIGHT_PURPLE);
-    m.add("LIGHT_LIME", color::consts::LIGHT_LIME);
-    m.add("LIGHT_ORANGE", color::consts::LIGHT_ORANGE);
-    m.add("LIGHT_TEAL", color::consts::LIGHT_TEAL);
-    m.add("LIGHT_PINK", color::consts::LIGHT_PINK);
-    m.add("VIVID_BLUE", color::consts::VIVID_BLUE);
+    m.add("WHITE", color::consts::WHITE.clone());
+    m.add("RED", color::consts::RED.clone());
+    m.add("BLUE", color::consts::BLUE.clone());
+    m.add("GREEN", color::consts::GREEN.clone());
+    m.add("BLACK", color::consts::BLACK.clone());
+    m.add("CYAN", color::consts::CYAN.clone());
+    m.add("YELLOW", color::consts::YELLOW.clone());
+    m.add("MAGENTA", color::consts::MAGENTA.clone());
+    m.add("PURPLE", color::consts::PURPLE.clone());
+    m.add("LIME", color::consts::LIME.clone());
+    m.add("ORANGE", color::consts::ORANGE.clone());
+    m.add("TEAL", color::consts::TEAL.clone());
+    m.add("PINK", color::consts::PINK.clone());
+    m.add("DARK_RED", color::consts::DARK_RED.clone());
+    m.add("DARK_BLUE", color::consts::DARK_BLUE.clone());
+    m.add("DARK_GREEN", color::consts::DARK_GREEN.clone());
+    m.add("DARK_CYAN", color::consts::DARK_CYAN.clone());
+    m.add("DARK_YELLOW", color::consts::DARK_YELLOW.clone());
+    m.add("DARK_MAGENTA", color::consts::DARK_MAGENTA.clone());
+    m.add("DARK_PURPLE", color::consts::DARK_PURPLE.clone());
+    m.add("DARK_LIME", color::consts::DARK_LIME.clone());
+    m.add("DARK_ORANGE", color::consts::DARK_ORANGE.clone());
+    m.add("DARK_TEAL", color::consts::DARK_TEAL.clone());
+    m.add("DARK_PINK", color::consts::DARK_PINK.clone());
+    m.add("LIGHT_RED", color::consts::LIGHT_RED.clone());
+    m.add("LIGHT_BLUE", color::consts::LIGHT_BLUE.clone());
+    m.add("LIGHT_GREEN", color::consts::LIGHT_GREEN.clone());
+    m.add("LIGHT_CYAN", color::consts::LIGHT_CYAN.clone());
+    m.add("LIGHT_YELLOW", color::consts::LIGHT_YELLOW.clone());
+    m.add("LIGHT_MAGENTA", color::consts::LIGHT_MAGENTA.clone());
+    m.add("LIGHT_PURPLE", color::consts::LIGHT_PURPLE.clone());
+    m.add("LIGHT_LIME", color::consts::LIGHT_LIME.clone());
+    m.add("LIGHT_ORANGE", color::consts::LIGHT_ORANGE.clone());
+    m.add("LIGHT_TEAL", color::consts::LIGHT_TEAL.clone());
+    m.add("LIGHT_PINK", color::consts::LIGHT_PINK.clone());
+    m.add("VIVID_BLUE", color::consts::VIVID_BLUE.clone());
     Ok(())
 }
